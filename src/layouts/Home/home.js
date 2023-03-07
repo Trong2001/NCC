181d32c8c901a731667514474e1f5cc082e0efa7
trongnx <trongnx@vnpt-technology.vn>
2023-03-07 21:24:22 +0700

    update lần 2

--- src/layouts/Home/home.js
+++ src/layouts/Home/home.js
@@ -1,4 +1,7 @@
 import "./index.css";
+import html from "../images/html-icon1.png";
+import css from "../images/css-icon1.png";
+import url from "../images/url-icon1.png";
 
 function home() {
   return (
@@ -19,7 +22,53 @@ function home() {
         </div>
       </div>
 
-      <div></div>
+      <div className="other">
+        <div className="html">
+          <div className="other-title">Lorem ipsum dolor sit amet</div>
+
+          <div className="other-content">
+            <img src={html} alt="Html-Icon" align="left" className="icon-anh" />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui
+            sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit
+            accumsan. Aenean consequat condimentum velit ut tempor. Nam porta
+            massa in metus bibendum congue. Pellentesque ultrices liquam egestas
+            nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel
+            sodales est ullamcorper id. Aenean consequat condimentum velit ut
+            tempor. Nam porta massa in metus bibendum congue. Pellentesque
+            ultrices vestibulum mattis.
+          </div>
+        </div>
+        <div className="css">
+          <div className="other-title">Lorem ipsum dolor sit amet</div>
+
+          <div className="other-content">
+            <img src={css} alt="Css-Icon" align="left" className="icon-anh" />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui
+            sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit
+            accumsan. Aenean consequat condimentum velit ut tempor. Nam porta
+            massa in metus bibendum congue. Pellentesque ultrices liquam egestas
+            nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel
+            sodales est ullamcorper id. Aenean consequat condimentum velit ut
+            tempor. Nam porta massa in metus bibendum congue. Pellentesque
+            ultrices vestibulum mattis.
+          </div>
+        </div>
+        <div className="url">
+          <div className="other-title">Lorem ipsum dolor sit amet</div>
+
+          <div className="other-content">
+            <img src={url} alt="url-Icon" align="left" className="icon-anh" />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui
+            sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit
+            accumsan. Aenean consequat condimentum velit ut tempor. Nam porta
+            massa in metus bibendum congue. Pellentesque ultrices liquam egestas
+            nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel
+            sodales est ullamcorper id. Aenean consequat condimentum velit ut
+            tempor. Nam porta massa in metus bibendum congue. Pellentesque
+            ultrices vestibulum mattis.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
