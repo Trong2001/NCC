25c8603cc1f8b72339ea3bfc1f7ba62697cb9758
trongnx <trongnx@vnpt-technology.vn>
2023-03-07 18:05:09 +0700

    Init ncc

---
+++ src/layouts/components/Sidebar/sidebar.js
@@ -0,0 +1,13 @@
+function sidebar() {
+  return (
+    <div className ="sidebar">
+      <div>Home</div>
+      <div>Services</div>
+      <div>News</div>
+      <div>Blog</div>
+      <div>Contact</div>
+    </div>
+  );
+}
+
+export default sidebar;
